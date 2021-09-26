3f405b439effed012a779a63f1ab12f36a3151c5
Ruslan <ruslan.shtybaev@gmail.com>
2021-09-27 01:44:39 +0300

    Create simple app

---
+++ TestAppWithApi/Cat.swift
@@ -0,0 +1,11 @@
+//
+//  Cat.swift
+//  TestAppWithApi
+//
+//  Created by Руслан Штыбаев on 27.09.2021.
+//
+
+
+struct Cat: Decodable {
+    let url: String?
+}
--- TestAppWithApi/ViewController.swift
+++ TestAppWithApi/ViewController.swift
@@ -8,12 +8,46 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var imageForCatView: UIImageView!
+    
+    @IBOutlet weak var getCatButton: UIButton!
+    
+    let link = "https://api.thecatapi.com/v1/images/search"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        fetchImage()
     }
+    
+    private func fetchImage() {
+        guard let url = URL(string: link) else { return }
+        
+        URLSession.shared.dataTask(with: url) { data, _, error in
+            guard let data = data else {
+                print(error?.localizedDescription ?? "No error description")
+                return
+            }
+            
+            do {
+                let courses = try JSONDecoder().decode(Cat.self, from: data)
+                print(courses)
+                guard let image = UIImage(data: data) else { return }
+                DispatchQueue.main.async {
+                    self.imageForCatView.image = image
+                }
+                
+            } catch let error {
+                print(error.localizedDescription)
+            }
+            
+        }.resume()
+            }
 
-
+    @IBAction func getCat() {
+        fetchImage()
+    }
+    
 }
 
