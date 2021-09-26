024c25cc3d4e8e29d15276cc098675d02cc783c9
Ruslan <ruslan.shtybaev@gmail.com>
2021-09-27 00:25:25 +0300

    Initial Commit

---
+++ TestAppWithApi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestAppWithApi
+//
+//  Created by Руслан Штыбаев on 27.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
